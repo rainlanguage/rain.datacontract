e361758f664c2b871b85bfc1694642abe56c4dda
David Meister <thedavidmeister@gmail.com>
2023-03-15 15:39:00 +0000

    wip on datacontract

--- src/DataContract.sol
+++ src/DataContract.sol
@@ -3,17 +3,19 @@ pragma solidity ^0.8.0;
 
 error WriteError();
 
+uint256 constant PREFIX_BYTES_LENGTH = 19;
+uint256 constant CURSOR_OFFSET = PREFIX_BYTES_LENGTH + 0x20;
+
 library DataContract {
     /// @return Pointer to the container that will be deployed.
     function allocate(uint256 length_) internal pure returns (bytes memory, uint256) {
         unchecked {
-            // 19 = number of prefix bytes
-            bytes memory container_ = new bytes(length_ + 19);
+            bytes memory container_ = new bytes(length_ + PREFIX_BYTES_LENGTH);
             uint256 cursor_;
             assembly ("memory-safe") {
-                // 39 = number of prefix bytes + 20 bytes for length slot
-                cursor_ := add(container_, 39)
+                cursor_ := container_
             }
+            cursor_ += CURSOR_OFFSET;
 
             // SSTORE2 Verbatim reference
             // https://github.com/0xsequence/sstore2/blob/master/contracts/utils/Bytecode.sol#L15
@@ -30,21 +32,11 @@ library DataContract {
             // However note that 00 is also prepended (although docs say append) so there's
             // an additional byte that isn't described above.
             // https://github.com/0xsequence/sstore2/blob/master/contracts/SSTORE2.sol#L25
-            bytes32 prefixBytes_ = hex"00000000000000000000000000_63_0000000000000000_80_60_0E_60_00_39_60_00_F3_00";
+            bytes32 prefix_ = bytes32(hex"0000000000000000000000000063000000000000000080600E6000396000F300")
+                | bytes32(uint256(uint32(length_ + 1)) << 80);
+
             assembly ("memory-safe") {
-                let prefix_ := or(
-                    prefixBytes_,
-                    shr(
-                        // Shift up to the spot where the length goes.
-                        80, 
-                        // Mask the length to 8 bytes.
-                        and(
-                            0xFFFFFFFFFFFFFFFF,
-                            // Account for the extra 00 byte in the prefix.
-                            add(length_, 1)
-                        )
-                    )
-                )
+                mstore(sub(cursor_, 0x20), prefix_)
             }
 
             return (container_, cursor_);
@@ -59,5 +51,20 @@ library DataContract {
         // Zero address means create failed.
         if (pointer_ == address(0)) revert WriteError();
         return pointer_;
+    }
+
+    function read(address pointer_) internal view returns (bytes memory) {
+        unchecked {
+            uint256 codesize_;
+            assembly ("memory-safe") { codesize_ := extcodesize(pointer_) }
+            uint256 offset_ = PREFIX_BYTES_LENGTH;
+            uint256 length_ = codesize_ - offset_;
+            bytes memory data_ = new bytes(length_);
+            assembly ("memory-safe") {
+                extcodecopy(pointer_, add(data_, 0x20), offset_, length_)
+            }
+            return data_;
+        }
+
     } 
 }
