769dc96fdd2a0898f9ea40e541735af75bc26ca0
David Meister <thedavidmeister@gmail.com>
2023-03-16 16:17:22 +0000

    add qakit

--- test/DataContract.t.sol
+++ test/DataContract.t.sol
@@ -1,34 +1,10 @@
 // SPDX-License-Identifier: CAL
 pragma solidity ^0.8.13;
 
-import "forge-std/Test.sol";
+import "qakit.foundry/QAKitMemoryTest.sol";
 import "../src/DataContract.sol";
 
-contract DataContractTest is Test {
-    function unsafeCopyBytesTo(uint256 inputCursor_, uint256 outputCursor_, uint256 remaining_) internal pure {
-        assembly ("memory-safe") {
-            for {} iszero(lt(remaining_, 0x20)) {
-                remaining_ := sub(remaining_, 0x20)
-                inputCursor_ := add(inputCursor_, 0x20)
-                outputCursor_ := add(outputCursor_, 0x20)
-            } { mstore(outputCursor_, mload(inputCursor_)) }
-
-            if gt(remaining_, 0) {
-                // Slither false positive here due to the variable shift of a
-                // constant value to create a mask.
-                let mask_ := shr(mul(remaining_, 8), 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF)
-                // preserve existing bytes
-                mstore(
-                    outputCursor_,
-                    or(
-                        // input
-                        and(mload(inputCursor_), not(mask_)),
-                        and(mload(outputCursor_), mask_)
-                    )
-                )
-            }
-        }
-    }
+contract DataContractTest is QAKitMemoryTest {
 
     function assertMemoryAlignment() internal {
         // Check alignment of memory after allocation.
