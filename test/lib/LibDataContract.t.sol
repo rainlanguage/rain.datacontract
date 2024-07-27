6d5b14afba65d1faa7e398be24aab33aebee54ff
thedavidmeister <thedavidmeister@gmail.com>
2024-07-27 20:15:48 +0400

    bump solmem

--- test/lib/LibDataContract.t.sol
+++ test/lib/LibDataContract.t.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: CAL
-pragma solidity =0.8.18;
+pragma solidity =0.8.25;
 
 import "../../lib/forge-std/src/Test.sol";
 import "../../lib/rain.solmem/src/lib/LibMemCpy.sol";
