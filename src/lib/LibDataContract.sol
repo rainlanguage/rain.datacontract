a54103d33a2e78e0407a1153e43ae269f769dea2
thedavidmeister <thedavidmeister@gmail.com>
2024-07-27 20:59:53 +0400

    lint

--- src/lib/LibDataContract.sol
+++ src/lib/LibDataContract.sol
@@ -55,51 +55,51 @@ type DataContractMemoryContainer is uint256;
 /// Solidity but instead requires the caller to copy memory directy by pointer.
 /// https://github.com/rainprotocol/sol.lib.bytes can help with that.
 library LibDataContract {
-    /// Prepares a container ready to write exactly `length_` bytes at the
+    /// Prepares a container ready to write exactly `length` bytes at the
     /// returned `pointer_`. The caller MUST write exactly the number of bytes
     /// that it asks for at the pointer otherwise memory WILL be corrupted.
-    /// @param length_ Caller specifies the number of bytes to allocate for the
+    /// @param length Caller specifies the number of bytes to allocate for the
     /// data it wants to write. The actual size of the container in memory will
     /// be larger than this due to the contract creation prefix and the padding
     /// potentially required to align the memory allocation.
-    /// @return container_ The pointer to the start of the container that can be
+    /// @return container The pointer to the start of the container that can be
     /// deployed as an onchain contract. Caller can pass this back to `write` to
     /// have the data contract deployed
     /// (after it copies its data to the pointer).
-    /// @return pointer_ The caller can copy its data at the pointer without any
+    /// @return pointer The caller can copy its data at the pointer without any
     /// additional allocations or Solidity type wrangling.
-    function newContainer(uint256 length_)
+    function newContainer(uint256 length)
         internal
         pure
-        returns (DataContractMemoryContainer container_, Pointer pointer_)
+        returns (DataContractMemoryContainer container, Pointer pointer)
     {
         unchecked {
-            uint256 prefixBytesLength_ = PREFIX_BYTES_LENGTH;
-            uint256 basePrefix_ = BASE_PREFIX;
+            uint256 prefixBytesLength = PREFIX_BYTES_LENGTH;
+            uint256 basePrefix = BASE_PREFIX;
             assembly ("memory-safe") {
                 // allocate output byte array - this could also be done without assembly
-                // by using container_ = new bytes(size)
-                container_ := mload(0x40)
+                // by using container = new bytes(size)
+                container := mload(0x40)
                 // new "memory end" including padding
-                mstore(0x40, add(container_, and(add(add(length_, prefixBytesLength_), 0x1f), not(0x1f))))
+                mstore(0x40, add(container, and(add(add(length, prefixBytesLength), 0x1f), not(0x1f))))
                 // pointer is where the caller will write data to
-                pointer_ := add(container_, prefixBytesLength_)
+                pointer := add(container, prefixBytesLength)
 
                 // copy length into the 2 bytes gap in the base prefix
-                let prefix_ :=
+                let prefix :=
                     or(
-                        basePrefix_,
+                        basePrefix,
                         shl(
                             // length sits 29 bytes from the right
                             232,
                             and(
                                 // mask the length to 2 bytes
                                 0xFFFF,
-                                add(length_, 1)
+                                add(length, 1)
                             )
                         )
                     )
-                mstore(container_, prefix_)
+                mstore(container, prefix)
             }
         }
     }
@@ -107,27 +107,27 @@ library LibDataContract {
     /// Given a container prepared by `newContainer` and populated with bytes by
     /// the caller, deploy to a new onchain contract and return the contract
     /// address.
-    /// @param container_ The container full of data to deploy as an onchain data
+    /// @param container The container full of data to deploy as an onchain data
     /// contract.
     /// @return The newly deployed contract containing the data in the container.
-    function write(DataContractMemoryContainer container_) internal returns (address) {
-        address pointer_;
-        uint256 prefixLength_ = PREFIX_BYTES_LENGTH;
+    function write(DataContractMemoryContainer container) internal returns (address) {
+        address pointer;
+        uint256 prefixLength = PREFIX_BYTES_LENGTH;
         assembly ("memory-safe") {
-            pointer_ :=
+            pointer :=
                 create(
                     0,
-                    container_,
+                    container,
                     add(
-                        prefixLength_,
+                        prefixLength,
                         // Read length out of prefix.
-                        and(0xFFFF, shr(232, mload(container_)))
+                        and(0xFFFF, shr(232, mload(container)))
                     )
                 )
         }
         // Zero address means create failed.
-        if (pointer_ == address(0)) revert WriteError();
-        return pointer_;
+        if (pointer == address(0)) revert WriteError();
+        return pointer;
     }
 
     /// Reads data back from a previously deployed container.
@@ -135,63 +135,63 @@ library LibDataContract {
     /// https://docs.soliditylang.org/en/v0.8.17/assembly.html#example
     /// Notable difference is that we skip the first byte when we read as it is
     /// a `0x00` prefix injected by containers on deploy.
-    /// @param pointer_ The address of the data contract to read from. MUST have
+    /// @param pointer The address of the data contract to read from. MUST have
     /// a leading byte that can be safely ignored.
-    /// @return data_ The data read from the data contract. First byte is skipped
+    /// @return data The data read from the data contract. First byte is skipped
     /// and contract is read completely to the end.
-    function read(address pointer_) internal view returns (bytes memory data_) {
-        uint256 size_;
+    function read(address pointer) internal view returns (bytes memory data) {
+        uint256 size;
         assembly ("memory-safe") {
             // Retrieve the size of the code, this needs assembly.
-            size_ := extcodesize(pointer_)
+            size := extcodesize(pointer)
         }
-        if (size_ == 0) revert ReadError();
+        if (size == 0) revert ReadError();
         assembly ("memory-safe") {
             // Skip the first byte.
-            size_ := sub(size_, 1)
+            size := sub(size, 1)
             // Allocate output byte array - this could also be done without
-            // assembly by using data_ = new bytes(size)
-            data_ := mload(0x40)
+            // assembly by using data = new bytes(size)
+            data := mload(0x40)
             // New "memory end" including padding.
             // Compiler will optimise away the double constant addition.
-            mstore(0x40, add(data_, and(add(add(size_, 0x20), 0x1f), not(0x1f))))
+            mstore(0x40, add(data, and(add(add(size, 0x20), 0x1f), not(0x1f))))
             // Store length in memory.
-            mstore(data_, size_)
+            mstore(data, size)
             // actually retrieve the code, this needs assembly
             // skip the first byte
-            extcodecopy(pointer_, add(data_, 0x20), 1, size_)
+            extcodecopy(pointer, add(data, 0x20), 1, size)
         }
     }
 
     /// Hybrid of address-only read, SSTORE2 read and Solidity docs.
     /// Unlike SSTORE2, reading past the end of the data contract WILL REVERT.
-    /// @param pointer_ As per `read`.
-    /// @param start_ Starting offset for reads from the data contract.
-    /// @param length_ Number of bytes to read.
-    function readSlice(address pointer_, uint16 start_, uint16 length_) internal view returns (bytes memory data_) {
-        uint256 size_;
+    /// @param pointer As per `read`.
+    /// @param start Starting offset for reads from the data contract.
+    /// @param length Number of bytes to read.
+    function readSlice(address pointer, uint16 start, uint16 length) internal view returns (bytes memory data) {
+        uint256 size;
         // uint256 offset and end avoids overflow issues from uint16.
-        uint256 offset_;
-        uint256 end_;
+        uint256 offset;
+        uint256 end;
         assembly ("memory-safe") {
             // Skip the first byte.
-            offset_ := add(start_, 1)
-            end_ := add(offset_, length_)
+            offset := add(start, 1)
+            end := add(offset, length)
             // Retrieve the size of the code, this needs assembly.
-            size_ := extcodesize(pointer_)
+            size := extcodesize(pointer)
         }
-        if (size_ < end_) revert ReadError();
+        if (size < end) revert ReadError();
         assembly ("memory-safe") {
             // Allocate output byte array - this could also be done without
-            // assembly by using data_ = new bytes(size)
-            data_ := mload(0x40)
+            // assembly by using data = new bytes(size)
+            data := mload(0x40)
             // New "memory end" including padding.
             // Compiler will optimise away the double constant addition.
-            mstore(0x40, add(data_, and(add(add(length_, 0x20), 0x1f), not(0x1f))))
+            mstore(0x40, add(data, and(add(add(length, 0x20), 0x1f), not(0x1f))))
             // Store length in memory.
-            mstore(data_, length_)
+            mstore(data, length)
             // actually retrieve the code, this needs assembly
-            extcodecopy(pointer_, add(data_, 0x20), offset_, length_)
+            extcodecopy(pointer, add(data, 0x20), offset, length)
         }
     }
 }
